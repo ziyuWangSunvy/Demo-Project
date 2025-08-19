f618bd97056ff2a527a82900dbe7e46fd706426e
wangziyu <ziyu.wang@sunvy.co.jp>
2025-08-19 16:04:53 +0800

    feat: demo 测试项目 ci 流程

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Ziyu Wang
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        int a = 1 + 1;
+        return "hello world";
+    }
+
+    @RequestMapping("/hello1")
+    public String hello1() {
+        int a = 1 + 1;
+        return "hello world";
+    }
+}
